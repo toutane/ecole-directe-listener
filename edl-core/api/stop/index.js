5107f290a63c3072a367ab311fa08daad28709a5
Charles-Antoine LEGER <ca@leger.email>
2020-05-24 17:12:44 +0200

    Create stop api to stop listening

--- edl-core/api/stop/index.js
+++ edl-core/api/stop/index.js
@@ -1,7 +1,20 @@
+const fetch = require("node-fetch");
+
+import { token } from "../../.config/APItoken";
+
 module.exports = (req, res) => {
-  stopListening(req.query).then(() => res.send({ status: "good" }));
+  deleteCronJob(req.query, res);
 };
 
-stopListening = (query) => {
-  return console.log(`Stoping to listen eleve: ${query.eleveId}`);
-};
+async function deleteCronJob(query, res) {
+  let cron = `https://www.easycron.com/rest`;
+
+  let response = await fetch(`${cron}/delete?token=${token}&id=${query.id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/edn",
+    },
+  });
+  let result = await response.json();
+  res.send(result);
+}
