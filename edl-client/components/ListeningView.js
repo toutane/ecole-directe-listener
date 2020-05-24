ffc31ee9581e77881e36d94b2b6e104854213792
Charles-Antoine LEGER <ca@leger.email>
2020-05-24 22:51:02 +0200

    Add listening view component

---
+++ edl-client/components/ListeningView.js
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import { ListenContext } from "../contexts/listenContext";
+import ControlButton from "./ControlButton";
+
+export default function ListeningView() {
+  const { isListening, cronId } = useContext(ListenContext);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.hr} />
+      <Feather
+        name="headphones"
+        size={40}
+        style={{ marginBottom: 10, color: "#ccc" }}
+      />
+      <Text style={styles.text}>
+        {isListening
+          ? `→ You are listening with Cron ID: ${cronId}`
+          : "Start to listen to EDL Core:"}
+      </Text>
+      <ControlButton />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingHorizontal: 40,
+    paddingVertical: 30,
+  },
+  hr: {
+    height: 1.5,
+    width: "100%",
+    backgroundColor: "rgb(242, 242, 247)",
+    marginBottom: 15,
+  },
+  text: {
+    fontSize: 15,
+    color: "rgba(96,100,109, 0.5)",
+    lineHeight: 24,
+    textAlign: "center",
+  },
+});
