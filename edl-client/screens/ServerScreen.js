d415e291a8f446870ae496c563ca90543182a9b4
Charles-Antoine LEGER <ca@leger.email>
2020-05-31 23:44:31 +0200

    added expo linking dependency + cleaning

--- edl-client/screens/ServerScreen.js
+++ edl-client/screens/ServerScreen.js
@@ -1,10 +1,22 @@
 import React, { useContext } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
+import ServerLogsCard from "../components/Server/ServerLogs/ServerLogsCard";
+import ServerStatusCard from "../components/Server/ServerStatus/ServerStatusCard";
+
 export default function ServerScreen() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <ServerStatusCard />
+      <ServerLogsCard />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  conatiner: {},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
