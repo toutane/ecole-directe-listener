0501a652832fe21405ea56224558511bd48319d1
Charles-Antoine LEGER <ca@leger.email>
2020-06-01 23:29:25 +0200

    Minor UIX cleans on screens

--- edl-client/screens/UserScreen.js
+++ edl-client/screens/UserScreen.js
@@ -1,12 +1,23 @@
 import React, { useContext } from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+
+import { PermissionsContext } from "../contexts/permissionsContext";
 
 import UserActionsView from "../components/User/UserActionsView";
+import SignOutButton from "../components/Auth/SignOutButton";
+import NotificationAlert from "../components/Alert/NotificationAlert";
 
 export default function UserScreen() {
+  const { isNotificationsGranted } = useContext(PermissionsContext);
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.alert}>
+        {isNotificationsGranted ? <NotificationAlert /> : null}
+      </View>
       <UserActionsView />
+      <View style={styles.button}>
+        <SignOutButton />
+      </View>
     </SafeAreaView>
   );
 }
@@ -14,8 +25,8 @@ export default function UserScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
   },
+  button: { bottom: 30, position: "absolute" },
+  alert: { paddingHorizontal: 30 },
 });
