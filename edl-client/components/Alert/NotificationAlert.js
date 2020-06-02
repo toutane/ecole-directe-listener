e5981a64fbf2db65f132de44ccd322d84d566695
Charles-Antoine LEGER <ca@leger.email>
2020-06-02 14:25:58 +0200

    Minor position update on UI

--- edl-client/components/Alert/NotificationAlert.js
+++ edl-client/components/Alert/NotificationAlert.js
@@ -1,5 +1,11 @@
 import React, { useContext } from "react";
-import { Linking, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Linking,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 export default function NotificationAlert() {
@@ -7,12 +13,14 @@ export default function NotificationAlert() {
     <TouchableOpacity
       style={styles.alert}
       onPress={() => Linking.openURL("app-settings:")}
-      activeOpacity={0.5}
+      activeOpacity={0.8}
     >
       <Feather name="alert-triangle" size={35} style={styles.icon} />
       <Text style={styles.text}>
-        To take full advantage of EDL Client functionality, you must activate
-        notifications: (Tap ths notif to activate)
+        {`To take full advantage of EDL Client functionality, activate notifications:\n\n`}
+        <Text style={[styles.text, { fontWeight: "700" }]}>
+          Tap this notif to activate →
+        </Text>
       </Text>
     </TouchableOpacity>
   );
@@ -23,11 +31,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#FBD38D",
-    width: "95%",
     height: 100,
     borderRadius: 20,
     paddingHorizontal: 20,
   },
-  text: { color: "#C05621", fontSize: 15 },
-  icon: { color: "#C05621", marginRight: 10 },
+  text: { color: "#C05621", fontSize: 15, flexShrink: 10 },
+  icon: { color: "#C05621", marginRight: 15 },
 });
